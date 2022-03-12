d42344da0b5b05875f22d82f49b9a83cb63865ef
murgendrag <murgendra67@gmail.com>
2022-03-13 00:03:16 +0530

    List added

---
+++ Rice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoJson
+{
+    public class Rice
+    {
+        public List<TypesOfRice> typesofrice;
+        public List<Typesofgrain> typesofgrain;
+        public List<TypesofWheat> typesofwheat;
+    }
+    public class TypesOfRice
+    {
+            public string Name { get; set; }
+            public int Weight { get; set; }
+            public int Price { get; set; }
+    }
+    public class Typesofgrain
+    {
+        public string Name { get; set; }
+        public  int Weight { get; set; }
+        public int Price { get; set; }
+    }
+    public class TypesofWheat
+    { 
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+    }
+
+
+}
