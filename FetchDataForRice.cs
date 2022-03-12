7acd720d4a04549523898dc3fc6afe0deeb8d1e6
murgendrag <murgendra67@gmail.com>
2022-03-13 00:16:12 +0530

    try catch used

---
+++ FetchDataForRice.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace DemoJson
+{
+    public class FetchDataForRice
+    {
+        public Rice Read(string Path)
+        {
+            using (StreamReader file = new StreamReader(Path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+                }catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
